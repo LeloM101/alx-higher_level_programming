15be749dc8b01dab7bb73c9fc7772b4c66a64c87
Nompumelelo Malinga <111236389+LeloM101@users.noreply.github.com>
2022-12-11 17:21:12 +0200

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 
 ''' Write a program that prints numbers from 0 to 99 '''
 for i in range(0, 99):
-        print("{0:0=2d}, ".format(i), end="")
+        print("{:02}, ".format(i), end="")
 print("{}\n".format(99))
