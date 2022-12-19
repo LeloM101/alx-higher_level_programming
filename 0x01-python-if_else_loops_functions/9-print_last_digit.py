71a74203511b0ec4795d0fc8295f7f6a4a85b047
Nompumelelo Malinga <111236389+LeloM101@users.noreply.github.com>
2022-12-19 18:10:03 +0200

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-  
+  last_digit = abs(number) % 10
+  if last_digit == 0
+    return(0)
+    print("0")
+  else:
+    return(last_digit)
+    print(last_digit)
